1c50728439f4eec46b610d88c99eed3034a0b884
Jeffsummers2412 <Jeffsummers2412@yahoo.com>
2018-06-18 16:41:33 -0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SigninPage } from '../pages/signin/signin';
+import { MyComponent } from '../components/foo';
+import { DisplayPage } from '../pages/display/display';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    MyComponent,
+    DisplayPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    MyComponent,
+    DisplayPage
   ],
   providers: [
     StatusBar,
